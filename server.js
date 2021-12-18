f452f93d5d1a9ef6c6159666d8505c0a58ef4e4c
Sloan <Sloan.boyce@gmail.com>
2021-12-18 14:31:07 -0500

    added html, client side js file and style sheet

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 app.listen(PORT, () => {
