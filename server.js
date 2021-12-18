f3aa3453be48ecd6b6b36a8c0b8e4491cce93312
Sloan <Sloan.boyce@gmail.com>
2021-12-18 14:38:36 -0500

    added paths that send html and css to client, passed test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'src')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './src/index.html'));
 });
