d3ee38d2102f0013cf1e8a7b778756c96ebc8316
Sloan <Sloan.boyce@gmail.com>
2021-12-18 22:19:47 -0500

    added reading and writing to JSON

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
-let noteObj = {
-    table: []
-};
+const data = loadJSON('db.json');
 
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(bodyParser.json());
@@ -17,8 +15,35 @@ app.get('/', (req, res) => {
 
 app.post('/add-note', (req, res) => {
     console.log(req.body);
+    createNewNote(req.body);
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
+    initNotes();
 });
+
+function initNotes() {
+    data.table.forEach(element => {
+        console.log(element);
+    });
+}
+
+function createNewNote(obj) {
+    data.table.push(obj);
+    saveJSON('db.json', data);
+    console.log(data.table);
+}
+
+function loadJSON(filename = '') {
+    return JSON.parse(
+        fs.existsSync(filename)
+            ? fs.readFileSync(filename).toString()
+            : '""'
+    )
+}
+
+function saveJSON(filename, json = '') {
+    fs.writeFileSync(filename, JSON.stringify(json, null, 2));
+}
+
